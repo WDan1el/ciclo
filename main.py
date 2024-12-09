e4ebba92700b1f72ee1261c697f7d0b75bd847ab
WDan1el <189869788+WDan1el@users.noreply.github.com>
2024-12-09 11:59:43 -0500

    ejercicio 8 terminado por completo

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-tamaño = int(input("Ingresa el tamaño del triángulo (altura): "))
-
+tamaño = int(input("Ingresa el tamaño del hexágono (un número entero positivo): "))
 
 if tamaño <= 0:
     print("El tamaño debe ser un número entero positivo.")
 else:
-    print("Aquí está tu triángulo:")
- 
+    print("Aquí está tu hexágono:")
+
+
     for i in range(1, tamaño + 1):
-        print("*" * i)
+        espacios = tamaño - i
+        print(" " * espacios + "*" * (tamaño + (i - 1) * 2))
+
+
+    for i in range(tamaño - 1, 0, -1):
+        espacios = tamaño - i
+        print(" " * espacios + "*" * (tamaño + (i - 1) * 2))
