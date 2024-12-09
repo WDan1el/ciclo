45d60fa967e3f38c4ae428ffe628e758deda9495
WDan1el <189869788+WDan1el@users.noreply.github.com>
2024-12-09 11:56:14 -0500

    ejercicio 7 terminado

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-total_minutos = 0
-
-print("Ingrese los tiempos de los tramos del viaje en minutos (ingrese 0 para finalizar):")
-
-while True:
-    tiempo = int(input("Duración del tramo (en minutos): "))
-    if tiempo == 0: 
-        break
-    if tiempo < 0:  
-        print("Por favor, ingrese un valor positivo.")
-        continue
-    total_minutos += tiempo
+altura = int(input("Ingresa la altura del rectángulo: "))
+ancho = int(input("Ingresa el ancho del rectángulo: "))
 
 
-horas = total_minutos // 60
-minutos = total_minutos % 60
+if altura <= 0 or ancho <= 0:
+    print("La altura y el ancho deben ser números enteros positivos.")
+else:
+    print("Aquí está tu rectángulo:")
 
-print(f"El tiempo total de viaje es: {horas} horas y {minutos} minutos.")
+    for _ in range(altura):
+        print("*" * ancho)
+##
