82ab5f9f30b2dedebb7ff2faf3029eb161489828
WDan1el <189869788+WDan1el@users.noreply.github.com>
2024-12-09 12:07:11 -0500

    ejerci 12 terminado

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import math
 
-e_approx = 0 
-n = 10  #!
-diferencia = 1  #
-umbral = 0.0001  
+# Inicializar variables
+e_approx = 0  # Estimación de e
+n = 10  # Comenzamos con 10!
+diferencia = 1  # Inicializar diferencia alta para entrar al bucle
+umbral = 0.0001  # Diferencia mínima entre sumandos consecutivos
 
+# Realizar la suma de los factoriales hasta que la diferencia sea menor que el umbral
 while diferencia >= umbral:
-   
+    
     factorial = math.factorial(n)
-   
+    
     e_approx += 1 / factorial
     
-   
     if n > 10:
         diferencia = abs(1 / factorial - 1 / math.factorial(n - 1))
     else:
         diferencia = 1  
+    
     n += 1
 
 print(f"El valor aproximado de e es: {e_approx + 2.71828}")  
